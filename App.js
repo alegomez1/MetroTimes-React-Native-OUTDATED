13c8de094b182a5f76e489f72815e8ab5c2d4a54
alejandrogomez <alegomez1@me.com>
2019-10-27 00:45:03 -0400

    added proper state and data handling to each train, update function works as well

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {
   View,
   Text,
   StatusBar,
+  Dimensions,
 } from 'react-native';
 
 import axios from 'axios';
@@ -32,19 +33,27 @@ class App extends React.Component {
     dadelandNB2: '',
     dadelandSB1: '',
     dadelandSB2: '',
+    brickellNB1: '',
+    brickellNB2: '',
+    brickellSB1: '',
+    brickellSB2: '',
   };
 
   async componentDidMount() {
     await axios
       .get('https://miami-transit-api.herokuapp.com/api/TrainTracker.json')
       .then(response => {
-        console.log('response----', response);
+        console.log('response----', response.data.RecordSet.Record);
         const data = response.data.RecordSet.Record;
         this.setState({
           dadelandNB1: data[6].NB_Time1,
           dadelandNB2: data[6].NB_Time2,
           dadelandSB1: data[6].SB_Time1,
           dadelandSB2: data[6].SB_Time2,
+          brickellNB1: data[1].NB_Time1,
+          brickellNB2: data[1].NB_Time2,
+          brickellSB1: data[1].SB_Time1,
+          brickellSB2: data[1].SB_Time2,
         });
       });
   }
@@ -61,6 +70,10 @@ class App extends React.Component {
           dadelandNB2: data[6].NB_Time2,
           dadelandSB1: data[6].SB_Time1,
           dadelandSB2: data[6].SB_Time2,
+          brickellNB1: data[1].NB_Time1,
+          brickellNB2: data[1].NB_Time2,
+          brickellSB1: data[1].SB_Time1,
+          brickellSB2: data[1].SB_Time2,
         });
       });
   };
@@ -71,18 +84,24 @@ class App extends React.Component {
         <View>
           <Text style={styles.header}>Dadeland</Text>
           <Text style={styles.subHeader}>NorthBound</Text>
-          <Text style={styles.trainText}>1st Train:</Text>
-          <Text style={styles.trainText}>2nd Train:</Text>
+          <Text style={styles.trainText}>1st Train: {this.state.dadelandNB1}</Text>
+          <Text style={styles.trainText}>2nd Train: {this.state.dadelandNB2}</Text>
           <Text style={styles.subHeader}>SouthBound</Text>
+          <Text style={styles.trainText}>1st Train: {this.state.dadelandSB1}</Text>
+          <Text style={styles.trainText}>2nd Train: {this.state.dadelandSB2}</Text>
           <Text style={styles.header}>Brickell</Text>
-          <Text style={styles.trainText}>1st Train:</Text>
-          <Text style={styles.trainText}>2nd Train:</Text>
           <Text style={styles.subHeader}>NorthBound</Text>
+          <Text style={styles.trainText}>1st Train: {this.state.brickellNB1}</Text>
+          <Text style={styles.trainText}>2nd Train: {this.state.brickellNB2}</Text>
           <Text style={styles.subHeader}>SouthBound</Text>
+          <Text style={styles.trainText}>1st Train: {this.state.brickellSB1}</Text>
+          <Text style={styles.trainText}>2nd Train: {this.state.brickellSB2}</Text>
         </View>
-        <Text style={styles.button} onPress={() => this.update()}>
-          Update
-        </Text>
+        <View style={styles.buttonContainer}>
+          <Text style={styles.button} onPress={() => this.update()}>
+            Update
+          </Text>
+        </View>
       </View>
     );
   }
@@ -101,24 +120,36 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingLeft: 20,
     // textAlign: "center",
-    color: 'white'
+    color: 'white',
   },
   subHeader: {
     fontSize: 30,
     paddingTop: 10,
     paddingLeft: 20,
-    color: 'white'
+    color: 'white',
   },
   trainText: {
     fontSize: 20,
     paddingTop: 10,
     paddingLeft: 20,
-    color: 'white'    
+    color: 'white',
   },
   button: {
     fontSize: 20,
     borderWidth: 2,
-    backgroundColor: 'red',
+    backgroundColor: 'white',
+    width: 100,
+    height: 50,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    // backgroundColor: 'blue',
+    marginTop: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: Dimensions.get('window').width,
   },
 });
 
