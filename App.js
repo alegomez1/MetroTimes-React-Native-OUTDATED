2600022462701d7a5798310bee4a25cffbee3819
alejandrogomez <alegomez1@me.com>
2019-10-28 14:07:42 -0400

    removed picker, redoing with buttons

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import {
   Dimensions,
   Button,
   TouchableOpacity,
-  Picker
+  Picker,
 } from 'react-native';
 
 import RNPickerSelect from 'react-native-picker-select';
@@ -26,57 +26,52 @@ import {
 
 class App extends React.Component {
   state = {
-station: '',
-stationName: '',
-firstNorthTrain: '',
-firstSouthTrain: '',
+    station: '',
+    stationName: '',
+    firstNorthTrain: '',
+    secondNorthTrain:'',
+    firstSouthTrain: '',
+    secondSouthTrain:'',
   };
 
-componentDidMount() {
+  componentDidMount() {}
 
-  }
+  update = async value => {
+    console.log('update func, value--->', value);
 
-  update = async(value) => {
-    console.log('update func, value--->', value)
-
-    axios.get(`http://miami-transit-api.herokuapp.com/api/TrainTracker.json?StationID=${value}`).then(
-        response  => {
-          let data = response.data.RecordSet.Record
-        console.log('response--', data)
+    axios
+      .get(
+        `http://miami-transit-api.herokuapp.com/api/TrainTracker.json?StationID=${value}`,
+      )
+      .then(response => {
+        let data = response.data.RecordSet.Record;
+        console.log('response--', data);
         this.setState({
           firstNorthTrain: data.NB_Time1,
-          firstSouthTrain: data.SB_Time1
-        })
-      }
-    )
+          firstSouthTrain: data.SB_Time1,
+        });
+      });
   };
 
   render() {
     return (
       <View style={styles.container}>
-           <Text style={styles.header}>Metro Times</Text>
-        <View style={styles.picker}>
-   
-        <RNPickerSelect 
-            onValueChange={(value) => this.update(value)}
-            items={[
-                { label: 'Dadeland North', value: 'DLN' },
-                { label: 'Brickell', value: 'BLK' },
-            ]}
-        />
-        </View> 
+        <Text style={[styles.header, styles.alignCenter]}>Metro Times</Text>
 
-        <View>
-          <Text>Station Name: {this.state.stationName}</Text>
-          <Text>1st North Train: {this.state.firstNorthTrain}</Text>
-          <Text>1st South Train: {this.state.firstSouthTrain}</Text>
+        <View style={styles.trainInfoContainer}>
+          <Text style={styles.header2}>Station: </Text>
+          <Text style={styles.subHeader}>Northbound </Text>
+          <Text style={styles.trainText}>1st Train: </Text>
+          <Text style={styles.trainText}>2nd Train: </Text>
+          <Text style={styles.subHeader}>Southbound </Text>
+          <Text style={styles.trainText}>1st Train: </Text>
+          <Text style={styles.trainText}>2nd Train: </Text>
         </View>
 
-
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} title="Update" onPress={() => this.update()}>
+          {/* <TouchableOpacity style={styles.button} title="Update" onPress={() => this.update()}>
           <Text style={styles.buttonText}>Update</Text>
-          </TouchableOpacity>
+          </TouchableOpacity> */}
         </View>
       </View>
     );
@@ -87,11 +82,17 @@ const styles = StyleSheet.create({
   container: {
     // marginTop: 0,
     flex: 1,
-    // justifyContent: 'flex-start',
-    alignItems: 'flex-start',
+    justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#343A40',
     height: Dimensions.get('window').height,
-    
+  },
+  trainInfoContainer: {
+    // backgroundColor: 'red',
+    paddingLeft: 20,
+  },
+  alignCenter: {
+    textAlign: 'center',
   },
   header: {
     fontSize: 50,
@@ -100,16 +101,19 @@ const styles = StyleSheet.create({
     // textAlign: "center",
     color: 'white',
   },
+  header2: {
+    fontSize: 45,
+    paddingTop: 50,
+    color: 'white',
+  },
   subHeader: {
-    fontSize: 30,
+    fontSize: 40,
     paddingTop: 10,
-    paddingLeft: 20,
     color: 'white',
   },
   trainText: {
     fontSize: 20,
     paddingTop: 10,
-    paddingLeft: 20,
     color: 'white',
   },
   button: {
@@ -122,9 +126,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 20,
   },
-  buttonText:{
+  buttonText: {
     fontSize: 20,
-    fontWeight: "bold"
+    fontWeight: 'bold',
   },
   buttonContainer: {
     flex: 1,
@@ -134,12 +138,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: Dimensions.get('window').width,
   },
-  picker:{
+  picker: {
     fontSize: 60,
     marginLeft: 50,
     marginTop: 100,
-    textAlign: "center"
-  }
+    textAlign: 'center',
+  },
 });
 
 export default App;
